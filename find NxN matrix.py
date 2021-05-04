040222fa12ed53b2b1e765fcc5d083d2d5278ef8
DANUSHRAJ <60309916+DANUSHRAJ@users.noreply.github.com>
2021-05-04 22:36:06 +0530

    Add files via upload

---
+++ find NxN matrix.py
@@ -0,0 +1,7 @@
+for k in range(x-a+1):
+    for h in range(y-a+1):
+        for m in range(k,k+a):
+            for n in range(h,h+a):
+                print(l[m][n],end=' ')
+            print()
+        print()
